f338834df3b5c6a242c6c560dd610af22f82c71f
Emilio Martin Lundgaard Lopez <emilio@lundgaardlopez.com>
2018-05-14 22:43:54 +0200

    Replace deprecated rootPath for workspaceFolders

--- src/config/runCommand.js
+++ src/config/runCommand.js
@@ -4,7 +4,7 @@ const vscode = require('vscode');
 const config = {
     runCommand: {
         options: [],
-        cwd: vscode.workspace.rootPath,
+        cwd: vscode.workspace.workspaceFolders[0].uri.path ,
         command: 'git describe --dirty="-$USER"'
     }
 }
